8ab702b6dbbd3db80b38bb24e67248e49c1ff7f5
Assaf <assaf.l@tevel-tech.com>
2022-05-03 12:52:34 +0300

    blablabla

--- eve.py
+++ eve.py
@@ -1,3 +1,17 @@
 import bla
 import bla2
 print("Hello world!!!")
+
+
+Andrea Sabty mentioned you on an issue
+Tevel_Main
+  /  	
+TEV-1277
+bug New dashboard pick numbers update
+	Andrea Sabty 6:47 PM IDT
+@Assafliv - please add fix version to all issues assigned to QA. Otherwise, we cannot see them in QA board. In this case 22.5
+
+🙂
+
+ 
+
